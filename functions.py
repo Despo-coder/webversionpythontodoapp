7af6d74fce9b6f3e9d011c2cdb24522f330ff157
Despo_the_Coder <damion.wilson@gmail.com>
2023-09-04 19:23:12 -0400

    Inital Commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+def get_todos(filepath='todos.txt'):
+   with open(filepath,'r') as local_file:
+      todos_local=local_file.readlines()
+   return todos_local
+
+def write_todos(todos_arg, filepath='todos.txt'):
+   with open(filepath,'w') as file:
+     file.writelines(todos_arg)
+   
+
+if __name__ == "__main__":
+   print(get_todos) 
---
+++ web.py
@@ -0,0 +1,35 @@
+import  streamlit as sl
+from functions import get_todos, write_todos
+import os
+
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt','w') as file:
+        pass
+
+
+todos = get_todos()
+def addTodo():
+    todo=sl.session_state['new_todo']
+    todos.append(todo +'\n')
+    write_todos(todos)
+    sl.session_state['new_todo']=''
+
+
+sl.title('My Todo App')
+sl.subheader('Welcome to the app')
+sl.write('This app is to create list of things to do')
+
+for index, todo in enumerate(todos) :
+    checkbox = sl.checkbox(todo, key=index)
+    if checkbox:
+        todos.pop(index)
+        write_todos(todos)
+        del sl.session_state[index]
+        sl.experimental_rerun()
+
+
+
+sl.text_input(label='.',placeholder='Enter a Todo...' , on_change = addTodo, key='new_todo')
+
+sl.session_state
